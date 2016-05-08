36b4dc3e3418eb3846050dfe9a50604fd54a0774
Chuck Frain <linux@chuckfrain.net>
2016-05-07 23:56:39 -0400

    Updated file names for UAS presentation

--- single_file/gen_slides_final.sh
+++ single_file/gen_slides_final.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-asciidoctor -T ./asciidoctor-deck.js/templates/haml/ -b deckjs asciidoctor_presentation_calug.adoc -o asciidoctor_presentation_calug_final.html
+asciidoctor -T ./asciidoctor-deck.js/templates/haml/ -b deckjs asciidoctor_presentation_uas.adoc -o asciidoctor_presentation_uas_final.html
