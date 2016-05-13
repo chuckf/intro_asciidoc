ebca3d57c2f9f126ca20504d2207d6baf0f271d9
Chuck Frain <linux@chuckfrain.net>
2016-05-12 22:50:10 -0400

    updated files and some small changes to finalize the presentation

--- split_files/gen_slides_final.sh
+++ split_files/gen_slides_final.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-asciidoctor -T ./asciidoctor-deck.js/templates/haml/ -b deckjs asciidoctor_presentation_uas_multi.adoc -o asciidoctor_presentation_uas_final_multi.html
+# asciidoctor -T ./asciidoctor-deck.js/templates/haml/ -b deckjs asciidoctor_presentation_uas_multi.adoc -o asciidoctor_presentation_uas_final_multi.html
+asciidoctor -T ../single_file/asciidoctor-deck.js/templates/haml/ -b deckjs asciidoctor_presentation_uas_multi.adoc -o asciidoctor_presentation_uas_final_multi.html
